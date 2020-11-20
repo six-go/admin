d1fa882cf83b617d8cd6fd74f1b7faa910926f23
00Lyb00 <2992130764@qq.com>
2020-11-20 20:26:03 +0800

    add:配置antd-design按需引入

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// ant-design的样式
+import 'ant-design-vue/dist/antd.css'
+
+// ant-design按需引入
+// 例如：import { Button } from 'ant-design-vue';
+
 // 引入路由器对象
 import router from './router'
 // 引入状态管理对象
 import store from './store'
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
